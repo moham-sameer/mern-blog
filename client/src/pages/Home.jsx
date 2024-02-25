46de95f85ac3e0440d8a3f05655c65db6d27b83b
Mohammad Sameer <ms4492222@gmail.com>
2024-02-25 15:12:10 +0530

    created server for authentication

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      Home
+      Homes of many components
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
 import express from 'express'
-
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("MongoDB is connected")
+}).catch((error)=>{
+    console.log(error)
+})
 const app = express()
 
 app.listen(3000,console.log("server is listening on port 3000..."))
