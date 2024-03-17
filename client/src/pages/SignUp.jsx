820a3306e40aa6bb13b3c8b3088ea2dd69f3f51d
Mohammad Sameer <ms4492222@gmail.com>
2024-03-17 21:32:15 +0530

    connected signup page from the database

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,13 +1,26 @@
-import { Label, TextInput } from 'flowbite-react'
-import React from 'react'
+import { Button, Label, TextInput } from 'flowbite-react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-
+import axios from 'axios'
 const SignUp = () => {
+  const [inputData,setInputData] = useState('');
+  
+  const changeHandler = (e) =>{
+    setInputData({
+      ...inputData,[e.target.id]:e.target.value
+    })
+  }
+  const submitHandler = async(e)=>{
+    e.preventDefault()
+    const data = await axios.post('http://localhost:3000/api/auth/signup',inputData)
+    console.log(data)
+  }
+
   return (
     <div className='min-h-screen mt-20'>
-    <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center'>
+    <div className='flex gap-5 p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center'>
     {/*left side */}
-      <div>
+      <div className='flex-1'>
       <Link to="/" className='font-bold text-4xl dark:text-white'>
       <span className='px-2 py-1 bg-gradient-to-r from-indigo-500  via-purple-500 to-pink-500 rounded-lg text-white'>Sameer's</span>
       Blog
@@ -17,14 +30,31 @@ const SignUp = () => {
       </p>
       </div>
       {/*right side*/}
-      <div className=''>
-         <form>
+      <div className='flex-1'>
+         <form onSubmit={submitHandler} className='flex flex-col gap-5'>
           <div>
-            <Label value='Your username'  />
-            <TextInput type='text' placeholder='Username' 
+            <Label value='Your Username'  />
+            <TextInput onChange={changeHandler}   type='username' placeholder='Enter Your Username' 
              id='username'  />
           </div>
+          <div>
+            <Label value='Your Email'  />
+            <TextInput onChange={changeHandler} type='email' placeholder='Enter Your Email' 
+             id='email'  />
+          </div>
+          <div>
+            <Label value='Your Password'  />
+            <TextInput onChange={changeHandler} type='password' placeholder='Enter Your Password' 
+             id='password'  />
+          </div>
+           <Button gradientDuoTone='purpleToPink' type='submit' >Sign Up</Button>
          </form>
+         <div className='flex gap-2 text-sm mt-5'>
+          <span>Have an account?</span>
+          <Link to='/signin' className='text-blue-500'>
+            Sign in
+          </Link>
+         </div>
       </div>
     </div>
     </div>
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import cors from 'cors'
 dotenv.config()
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("MongoDB is connected")
@@ -10,8 +11,9 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(error)
 })
 const app = express()
-app.use(express.json())
+app.use(cors())
 
+app.use(express.json())
 app.use('/api/user',userRoutes)
 app.use('/api/auth',authRoutes)
 app.use((error,req,res, next)=>{
